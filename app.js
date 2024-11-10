8623b06ad1d75bb5fe500cc9821a739aee79d7da
Alexander Lindqvist <alexander@solitudedesign.se>
2024-11-10 15:16:45 +0100

    Add function to get random word from array

---
+++ app.js
@@ -0,0 +1,28 @@
+const qwerty = document.querySelector('#qwerty');
+const phrase = document.querySelector('#phrase');
+const overlay = document.querySelector('#overlay');
+const btnReset = document.querySelector('.btn__reset');
+
+let score = 0;
+
+// Phrases to guess for the game
+const phrases = [
+    'Forest whispers softly', 
+    'Golden sunlight warms',
+    'Rivers flow gently',
+    'Mountains stand tall',
+    'Flowers bloom bright'
+];
+
+// Event listner to the start game button
+btnReset.addEventListener('click', (e) => {
+    overlay.style.display = 'none'
+});
+
+// Function that returns a random array item from an array
+function getRandomPhraseAsArray (arr) {
+    let randomNumber = Math.floor(Math.random()*arr.length);
+    let randomPhrase = arr.slice(randomNumber, randomNumber + 1);
+
+    return randomPhrase;
+}
