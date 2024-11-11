33fe4659dbbf288fa59f86063d2bc29db014eb45
Alexander Lindqvist <alexander@solitudedesign.se>
2024-11-11 21:10:07 +0100

    Add working checkLetter function

--- app.js
+++ app.js
@@ -2,17 +2,14 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const overlay = document.querySelector('#overlay');
 const btnReset = document.querySelector('.btn__reset');
-let buttonText = "";
-
-let score = 0;
 
 // Phrases to guess for the game
 const phrases = [
-    // 'Forest whispers softly', 
-    // 'Golden sunlight warms',
-    // 'Rivers flow gently',
-    // 'Mountains stand tall',
-    'Flowers bloom bright'
+    // 'forest whispers softly', 
+    // 'golden sunlight warms',
+    // 'rivers flow gently',
+    // 'mountains stand tall',
+    'flowers bloom bright'
 ];
 
 // Event listner to the start game button.
@@ -56,7 +53,7 @@ function checkLetter (value) {
     let listItems = phrase.querySelector('ul').childNodes;
 
     // Create a variable to store if a match is found and give it an initial value of null
-    let match = null;
+    let match = [];
     
     // Loop through all of the li elements. Remember: arrays start with index 0!
 
@@ -65,9 +62,9 @@ function checkLetter (value) {
         // Create a conditional that compares the text of the button parameter to the text of the li at the current index of the loop
         if (value === listItems[i].textContent) {
             // If they match, add the “show” class to the li
-            li[i].className = 'show';
+            listItems[i].className = 'letter show';
             // If they match, store the button text in the match variable
-            // match.push(listItems[i].textContent());
+            match.push(listItems[i].textContent);
         }
     }
     // Once the loop completes, return the match variable
@@ -76,10 +73,11 @@ function checkLetter (value) {
 
 // Checks if a button in QWERTY is clicked and returns the text of that button
 qwerty.addEventListener('click', (e) => {
-    if (e.target.tagName === 'BUTTON') {
+    if (e.target.tagName === 'BUTTON' && e.target.className != "chosen") {
         buttonText = e.target.innerHTML;
+        e.target.className = "chosen";
         checkLetter(buttonText);
-    }
+    } 
 });
 
 
